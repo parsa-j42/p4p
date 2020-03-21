0419b880fe130088958e637afa6baf2aa9f4298b
dark-galaxy20 <62478677+dark-galaxy20@users.noreply.github.com>
2020-03-22 01:12:49 +0430

    t

---
+++ 1-Character-Input.py
@@ -0,0 +1,7 @@
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+# http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+year = 2020
+user_name = input("enter your name")
+user_age = int(input("enter your age"))
+x = 100 - user_age
