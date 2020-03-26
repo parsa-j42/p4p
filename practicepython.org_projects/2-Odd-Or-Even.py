ddb576a26fb6ef6493e467fa5aef0f781749c036
dark-galaxy20 <62478677+dark-galaxy20@users.noreply.github.com>
2020-03-26 19:52:38 +0430

    odd-or-even extras

--- practicepython.org_projects/2-Odd-Or-Even.py
+++ practicepython.org_projects/2-Odd-Or-Even.py
@@ -7,4 +7,24 @@ if num % 2 == 0:
     print("your number is even")
 else:
     print("your number is odd")
-    
+# Extras:
+# 1-If the number is a multiple of 4, print out a different message.
+
+num = int(input("please enter a number: "))
+if num % 4 == 0:
+    print("your number is made by 4(s)")
+elif num % 2 == 0:
+    print("your number is even")
+else:
+    print("your number is odd")
+
+
+# 2-Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+# If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+num = int(input("please enter a number : "))
+check = int(input("please enter the number you want to check by: "))
+if num % check == 0:
+    print("your number is made of %s" % check)
+else :
+    print("your number is NOT made of %s" % check)
