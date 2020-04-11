ad21d5d02e0e88845155d53fe4bc3e7ed6b03325
dark-galaxy20 <62478677+dark-galaxy20@users.noreply.github.com>
2020-04-11 19:16:17 +0430

    22

---
+++ practicepython.org-projects/14-list-remove-duplicates.py
@@ -0,0 +1,7 @@
+# Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+# Extras:
+
+# Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+# Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+# http://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
---
+++ practicepython.org-projects/22-read-from-file.py
@@ -0,0 +1,22 @@
+# Given a .txt file that has a list of a bunch of names, 
+# count how many of each name there are in the file,
+#  and print out the results to the screen. 
+# http://www.practicepython.org/exercise/2014/12/06/22-read-from-file.html
+read_file = open('22-read-from-file.txt', 'r')
+current_line = read_file.readline()
+dareth = 0
+luke = 0
+lea = 0
+other_names = 0
+while current_line:
+    if current_line == "Darth\n" or current_line == 'Darth':
+        dareth += 1
+    elif current_line == "Luke\n" or current_line == "Luke":
+        luke += 1
+    elif current_line == "Lea\n" or current_line == "Lea":
+        lea += 1
+    else:
+        other_names += 1
+    current_line = read_file.readline()
+print("luke = ",str(luke),"\nDareth = ",str(dareth),"\nlea = ",str(lea),"\nothername = ", str(other_names))
+input()
